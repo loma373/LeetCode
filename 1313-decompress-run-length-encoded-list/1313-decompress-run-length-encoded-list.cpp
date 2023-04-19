3eed19577603a50be998d63f1789853b331dd4a0
Amol Vyas <54383954+loma373@users.noreply.github.com>
2023-04-19 11:57:16 +0530

    Time: 7 ms (54.30%), Space: 10.1 MB (10.24%) - LeetHub

--- 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.cpp
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.cpp
@@ -4,9 +4,7 @@ public:
         vector<int> res;
         int n=nums.size();
         for(int i=0;i<n;i+=2){
-            for(int j=0;j<nums[i];j++){
-                res.push_back(nums[i+1]);
-            }
+            res.insert(res.end(),nums[i],nums[i+1]);
         }
         
         return res;
