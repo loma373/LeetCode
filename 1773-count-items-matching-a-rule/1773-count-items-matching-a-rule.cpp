956e142334bdd5754307447d5f32069e96bbfa38
Amol Vyas <54383954+loma373@users.noreply.github.com>
2023-04-25 10:53:46 +0530

    Time: 71 ms (85.63%), Space: 30.9 MB (82.09%) - LeetHub

---
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
+        int k;
+        if(ruleKey=="type")
+            k=0;
+        else if(ruleKey=="color")
+            k=1;
+        else
+            k=2;
+        
+        int res=0;
+        for(int i=0;i<items.size();i++){
+            if(items[i][k]==ruleValue)
+                res++;
+        }
+        
+        return res;
+    }
+};
